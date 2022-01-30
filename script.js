bf5e52efc76f4b7d6ed762b37bc5cdb24bf4d6c1
Adam Wojtysiak <wojtysiak_a@lynchburg.edu>
2022-01-29 20:29:32 -0500

    adjustments to layout in css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const genderArr = ['Male', 'Female'];
 const raceArr = ['Human', 'Human', 'Tiefling', 'Tiefling', 'Orc', 'Orc', 'Half Hum/Tief', 'Half Tief/Hum', 'Half Orc/Hum', 'Half Orc/Tief'];
 const heightArr = ['Tiny', 'Short', 'Short', 'Short', 'Average', 'Average', 'Average', 'Average', 'Tall', 'Tall', 'Tall', 'Gigantic'];
 const ageArr = ['Child', 'Teen', 'Young Adult', 'Young Adult', 'Adult', 'Adult', 'Adult', 'Late Adulthood', 'Elder'];
-const weightArr = ['Skeletal', 'Skinny', 'Skinny', 'Skinny', 'Slim', 'Slim', 'Slim', 'Average', 'Average', 'Average', 'Average', 'Overweight', 'Overweight', 'Overweight', 'Obese', 'Obese'];
+const weightArr = ['Skeletal', 'Skinny', 'Skinny', 'Skinny', 'Slim', 'Slim', 'Slim', 'Average', 'Average', 'Average', 'Average', 'Overweight', 'Overweight'];
 const hairArr = ['Short', 'Medium', 'Long'];
 const facialHairArr = ['Clean-Shaven', 'Clean-Shaven', 'Clean-Shaven', 'Stubble', 'Stubble', 'Stubble', 'Trimmed', 'Trimmed', 'Trimmed', 'Full-Beard', 'Full-Beard', 'Full-Beard', 'Thick-Beard', 'Thick-Beard', 'Huge-Beard'];
 const clothesArr = ['Dirt-Poor', 'Dirt-Poor', 'Dirt-Poor', 'Poor', 'Poor', 'Poor', 'Average', 'Average', 'Average', 'Middle-Class', 'Middle-Class', 'Middle-Class', 'Wealthy', 'Wealthy', 'Rich'];
@@ -109,17 +109,17 @@ function generateRandomDescriptor(descriptorArr) {
 }
 
 function fillDescriptorObj() {
-    this.gender = generateRandomDescriptor(genderArr);
-    this.race = generateRandomDescriptor(raceArr);
-    this.height = generateRandomDescriptor(heightArr);
-    this.age = generateRandomDescriptor(ageArr);
-    this.weight = generateRandomDescriptor(weightArr);
-    this.hair = generateRandomDescriptor(hairArr);
-    this.clothes = generateRandomDescriptor(clothesArr);
-    if (this.gender == 'Male' && this.age != 'Child' && this.age != 'Teen') {
-        this.facialHair = generateRandomDescriptor(facialHairArr);
+    this.gender = generateRandomDescriptor(genderArr).toUpperCase();
+    this.race = generateRandomDescriptor(raceArr).toUpperCase();
+    this.height = generateRandomDescriptor(heightArr).toUpperCase();
+    this.age = generateRandomDescriptor(ageArr).toUpperCase();
+    this.weight = generateRandomDescriptor(weightArr).toUpperCase();
+    this.hair = generateRandomDescriptor(hairArr).toUpperCase();
+    this.clothes = generateRandomDescriptor(clothesArr).toUpperCase();
+    if (this.gender == 'MALE' && this.age != 'CHILD' && this.age != 'TEEN') {
+        this.facialHair = generateRandomDescriptor(facialHairArr).toUpperCase();
     } else {
-        this.facialHair = 'None'
+        this.facialHair = 'NONE'
     }
 }
 
